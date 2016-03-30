7c0a33927c4a6efc8c482e819864a467a743fbe2
iTitus <iTitus@users.noreply.github.com>
2016-03-30 18:53:35 +0200

    Add graphics.

--- src/main/java/io/github/ititus/gimmestuff/block/BlockInfiniteFluid.java
+++ src/main/java/io/github/ititus/gimmestuff/block/BlockInfiniteFluid.java
@@ -2,17 +2,40 @@ package io.github.ititus.gimmestuff.block;
 
 import io.github.ititus.gimmestuff.tile.TileInfiniteFluid;
 
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.world.World;
+
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class BlockInfiniteFluid extends BlockBase {
 
 	public BlockInfiniteFluid() {
 		super("blockInfiniteFluid");
+		setLightOpacity(0);
 	}
 
 	@Override
 	public TileEntity createNewTileEntity(World world, int meta) {
 		return new TileInfiniteFluid();
 	}
+
+	@Override
+	@SideOnly(Side.CLIENT)
+	public BlockRenderLayer getBlockLayer() {
+		return BlockRenderLayer.CUTOUT;
+	}
+
+	@Override
+	public boolean isFullCube(IBlockState state) {
+		return false;
+	}
+
+	@Override
+	public boolean isOpaqueCube(IBlockState state) {
+		return false;
+	}
+
 }
--- src/main/java/io/github/ititus/gimmestuff/proxy/ClientProxy.java
+++ src/main/java/io/github/ititus/gimmestuff/proxy/ClientProxy.java
@@ -1,5 +1,26 @@
 package io.github.ititus.gimmestuff.proxy;
 
+import io.github.ititus.gimmestuff.GimmeStuff;
+import io.github.ititus.gimmestuff.init.ModBlocks;
+
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.item.Item;
+
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
 public class ClientProxy extends CommonProxy {
+
+	@Override
+	public void preInit(FMLPreInitializationEvent event) {
+		super.preInit(event);
+
+		ModelLoader.setCustomModelResourceLocation(Item.getItemFromBlock(ModBlocks.blockInfiniteFluid), 0, getModelResLoc(ModBlocks.blockInfiniteFluid.getName(), "inventory"));
+
+	}
+
+	private ModelResourceLocation getModelResLoc(String path, String variant) {
+		return new ModelResourceLocation(GimmeStuff.MOD_ID + ":" + path, variant);
+	}
 
 }
